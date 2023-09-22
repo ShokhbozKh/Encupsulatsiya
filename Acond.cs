5a9895b82c81344fc3e76d3968e0c5e671b4111d
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-22 10:18:04 +0500

    Add project files.

---
+++ Acond.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Acond
+    {
+        private string Name;
+        private string Password;
+
+        private decimal Money;
+
+        public Acond(string name, string password, decimal money)
+        {
+            Name = name;
+            Password = password;
+            Money= money; 
+        }
+
+        public void GetName(string name)//tashqaridan olingan malumotlarga
+        {
+            Name=name;
+
+        }
+        public string SetName()//olingan malumotni ichkariga uzlashtirish ga
+        {
+            return Name;    
+
+        }
+        public void GetPassword(string password)
+        {
+            Password = password;
+        }
+        public string SetPassword()
+        {
+            return Password;
+        }
+
+        public void SetMoney(decimal money)
+        {
+            Money += money;
+        }
+        public string GetMoney(string parol)
+        {
+            if(Password==parol)
+                return Money.ToString(); 
+            else return  "xato parol kiritildi";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acond acond1 = new Acond("Shohboz", "1234", 0);
+            acond1.SetMoney(123000);
+            acond1.SetMoney(123000);
+            acond1.SetMoney(123000);
+
+            Console.WriteLine( acond1.GetMoney("1234"));
+            Console.WriteLine(acond1.SetPassword());
+        }
+    }
+}
